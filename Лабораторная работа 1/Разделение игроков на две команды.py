b5a5628bcf6b23f5a0b9b5d06dce5a69dfbd8c93
Yuichir0 <50494670+Yuichir0@users.noreply.github.com>
2023-10-15 22:47:18 +0300

    Add files via upload
    Выполнена лабораторная работа 1

---
+++ Лабораторная работа 1/Разделение игроков на две команды.py
@@ -0,0 +1,6 @@
+list_players = ["Маша", "Петя", "Саша", "Оля", "Кирилл", "Коля"]
+
+playersNumber = len(list_players)
+
+print(list_players[0:playersNumber // 2])
+print(list_players[playersNumber // 2:playersNumber])
